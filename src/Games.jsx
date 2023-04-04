13ce7e847ec320c6b0fa5639c9598bf2da604f63
Julian McDonald <warcraft_666_2@hotmail.com>
2023-04-04 20:13:18 +1000

    Add apostrophes in gamesList

--- src/Games.jsx
+++ src/Games.jsx
@@ -1,23 +1,7 @@
-export const gamesList = [
-  "Counter-Strike",
-  "CoD",
-  "Medal of Honor",
-  "Doom",
-  "Unreal",
-  "Halo",
-  "Quake",
-  "Far Cry",
-  "Gears of War",
-  "Splatoon",
-  "Battlefield",
-];
 
-const Game = (games) => {
-  for (let i = 0; i < games.length; i++) {
-    console.log(games);
-    // return <div>{<h3>{games[i]}</h3>}</div>;
+const Game = () => {
+
   }
-  return;
 };
 
 export default Game;
