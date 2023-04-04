6c591da3919e7b285e755786a02be2499ebe124b
Julian McDonald <warcraft_666_2@hotmail.com>
2023-04-04 21:46:23 +1000

    Add the counter for number of games checked

--- src/Checklist.jsx
+++ src/Checklist.jsx
@@ -1,15 +1,25 @@
 import Game from "./Games";
+import { useState } from "react";
 
 const Checklist = () => {
+  const [gamesCount, setGamesCount] = useState(0);
+
+  const handleCheckboxChange = (checkedState) => {
+    const count = checkedState.filter(Boolean).length;
+    setGamesCount(count);
+  };
+
   return (
     <div>
-      <h1>What is your VARIETY gamer score?</h1>
-      <h2>
-        Which of these Video Game Series did you play (at least one game of?)
-        One point per box checked!
-      </h2>
-      <h2>/144</h2>
-      <Game />
+      <div>
+        <h1>What is your VARIETY gamer score?</h1>
+        <h2>
+          Which of these Video Game Series did you play (at least one game of?)
+          One point per box checked!
+        </h2>
+        <h2>{gamesCount}/144</h2>
+        <Game handleCheckboxChange={handleCheckboxChange} />
+      </div>
     </div>
   );
 };
--- src/Games.jsx
+++ src/Games.jsx
@@ -1,14 +1,38 @@
 import { gamesList } from "./gamesList";
+import { useState } from "react";
 
-const Game = () => {
-  const Games = gamesList.map((g) => (
+const Game = (props) => {
+  const [checkedState, setCheckedState] = useState(
+    new Array(gamesList.length).fill(false)
+  );
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+    props.handleCheckboxChange(updatedCheckedState);
+  };
+
+  return (
     <div>
-      <input type="checkbox" id="game" name="game" />
-      {g}
+      {gamesList.map((game, index) => {
+        return (
+          <div key={index}>
+            <input
+              type="checkbox"
+              id={`game-${index}`}
+              name="game"
+              value={game}
+              checked={checkedState[index]}
+              onChange={() => handleOnChange(index)}
+            />
+            <label htmlFor={`game-${index}`}>{game}</label>
+          </div>
+        );
+      })}
     </div>
-  ));
-
-  return Games;
+  );
 };
 
 export default Game;
